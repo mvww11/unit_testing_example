cabbe87da38ac9028ead4cf173900376df97635a
Marcus Vinicius Winograd Waddington Telles Ribeiro <marcus.waddington@radixeng.com.br>
2022-11-09 16:03:08 -0300

    Changed ec2 ingress rules to allow all protocols and ports

--- infra/main.tf
+++ infra/main.tf
@@ -51,9 +51,9 @@ resource "aws_security_group" "mtc_sg" {
   description = "public security group"
   vpc_id      = aws_vpc.mtc_vpc.id
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
